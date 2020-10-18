af46e28bc1c047f1558ef2aca32321946895c3f7
Yeem Chij Thao <yc.vangsouathao@hotmail.com>
2020-10-17 19:09:59 -0500

    Upading README, finished up script to run criteria validation and to show complete password with at least one criteria character showing

--- README.md
+++ README.md
@@ -6,8 +6,8 @@ Create an application that an employee can use to generate a random password bas
 
 User click button to generate a password.
 User is presented with a series of prompts for a password criteria.
-User is prompt to select the lenght of the password between 8 and 128 characters.
-User is prompt to choose at least one of the following for a password criteria which should includes lowercase, uppercase, numeric and/or special characters.
+First prompt to select the length of the password between 8 and 128 characters.
+Next come a series of prompts to choose at least one of the following criteria: lowercase, uppercase, numeric and/or special characters.
 When all prompts are choosen, generate a password to display on the page.
 
 ## Link ##
@@ -16,4 +16,18 @@ https://ycthao.github.io/passwordgenerator/
 
 ## Screenshot ##
 
+![image](https://user-images.githubusercontent.com/71569747/96355796-7f87f280-10ab-11eb-90c0-ab4591cf15bf.png)
+![image](https://user-images.githubusercontent.com/71569747/96355805-9d555780-10ab-11eb-973e-20f3ad515adc.png)
+![image](https://user-images.githubusercontent.com/71569747/96355812-b4944500-10ab-11eb-8d50-b5e1ee3d2f92.png)
+![image](https://user-images.githubusercontent.com/71569747/96355817-c4138e00-10ab-11eb-8251-ec79717a7294.png)
+![image](https://user-images.githubusercontent.com/71569747/96355821-cfff5000-10ab-11eb-9016-61ec70cac39e.png)
+![image](https://user-images.githubusercontent.com/71569747/96355828-e7d6d400-10ab-11eb-84bf-6f596c20f0a4.png)
+![image](https://user-images.githubusercontent.com/71569747/96355835-148aeb80-10ac-11eb-995a-05ac845b1f93.png)
 
+## Installing ##
+Not required if opened via link provided.
+If not using the link, download zip via GitHub. 
+Unzip file downloaded, then open index.html to view.
+
+## Coding ##
+Uses: HTML, CSS, JavaScript
--- script.js
+++ script.js
@@ -23,9 +23,6 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-
-
-
 // Adding generatePassword
 function generatePassword(){
 
@@ -38,16 +35,20 @@ function generatePassword(){
     passwordLength = prompt("Please choose the length of password between 8 to 128 characthers: ");
   }
 
+  let charReqLength = 0;
+  let charReq = "";
   let passwordchar = "";
-
-
   let randomPassword = "";
   let setPassArr = "";
 
   // prompt to ask for lowercase
   let lowercase = confirm("Would you like your password to include lowercase?");
   if (lowercase) {
-    setPassArr = setPassArr + setPassArr.concat(lowercaseArr);
+    setPassArr = setPassArr.concat(lowercaseArr);
+    // lowercase is selected
+    let lowerReq = lowercaseArr[Math.floor(Math.random() * lowercaseArr.length)];
+    charReq = charReq.concat(lowerReq);
+    charReqLength++;
     alert("You choose lowercase.");
   } else {
     alert("You didn't choose lowercae.")
@@ -56,7 +57,11 @@ function generatePassword(){
   // prompt to ask for uppercase
   let uppercase = confirm("Would you like your password to include uppercase?");
   if (uppercase) {
-    setPassArr = setPassArr + setPassArr.concat(uppercaseArr);
+    setPassArr = setPassArr.concat(uppercaseArr);
+    // uppercase is selected
+    let upperReq = uppercaseArr[Math.floor(Math.random() * uppercaseArr.length)];
+    charReq = charReq.concat(upperReq);
+    charReqLength++;
     alert("You choose uppercase.");
   } else {
     alert("You didn't choose uppercase.")
@@ -65,7 +70,11 @@ function generatePassword(){
   // prompt to ask for numeric
   let numeric = confirm("Would you like your password to include numbers?")
   if (numeric) {
-    setPassArr = setPassArr + setPassArr.concat(numbericArr);
+    setPassArr = setPassArr.concat(numbericArr);
+    // number is selected
+    let numbReq = numbericArr[Math.floor(Math.random() * numbericArr.length)];
+    charReq = charReq.concat(numbReq);
+    charReqLength++;
     alert("You choose number.");
   } else {
     alert("You didn't choose number.")
@@ -74,51 +83,71 @@ function generatePassword(){
   // prompt to ask for special characters
   let specialCharacters = confirm("Would you like your password to include special characters?")
   if (specialCharacters) {
-    setPassArr = setPassArr + setPassArr.concat(specialCharactersArr);
+    setPassArr = setPassArr.concat(specialCharactersArr);
+    // special character is selected
+    let specialCharReq = specialCharactersArr[Math.floor(Math.random() * specialCharactersArr.length)];
+    charReq = charReq.concat(specialCharReq);
+    charReqLength++;
     alert("You choose special characters.");
   } else {
     alert("You didn't choose special characters.")
   }
 
   // Validation if user doesn't pick a criteria
-  while(!lowercase) {
+  while(!lowercase && !uppercase && !numeric && !specialCharacters) {
     alert("Try again, choose at least one criteria.");
 
-    // prompt to ask for lowercase
-    let lowercase = confirm("Would you like your password to include lowercase?");
-    if (lowercase) {
-      setPassArr = setPassArr + setPassArr.concat(lowercaseArr);
-      alert("You choose lowercase.");
-    } else {
-      alert("You didn't choose lowercae.")
-    }
+  // prompt to ask for lowercase
+  let lowercase = confirm("Would you like your password to include lowercase?");
+  if (lowercase) {
+    setPassArr = setPassArr.concat(lowercaseArr);
+    // lowercase is selected
+    let lowerReq = lowercaseArr[Math.floor(Math.random() * lowercaseArr.length)];
+    charReq = charReq.concat(lowerReq);
+    charReqLength++;
+    alert("You choose lowercase.");
+  } else {
+    alert("You didn't choose lowercae.")
+  }
 
-    // prompt to ask for uppercase
-    let uppercase = confirm("Would you like your password to include uppercase?");
-    if (uppercase) {
-      setPassArr = setPassArr + setPassArr.concat(uppercaseArr);
-      alert("You choose uppercase.");
-    } else {
-      alert("You didn't choose uppercase.")
-    }
+  // prompt to ask for uppercase
+  let uppercase = confirm("Would you like your password to include uppercase?");
+  if (uppercase) {
+    setPassArr = setPassArr.concat(uppercaseArr);
+    // uppercase is selected
+    let upperReq = uppercaseArr[Math.floor(Math.random() * uppercaseArr.length)];
+    charReq = charReq.concat(upperReq);
+    charReqLength++;
+    alert("You choose uppercase.");
+  } else {
+    alert("You didn't choose uppercase.")
+  }
 
-    // prompt to ask for numeric
-    let numeric = confirm("Would you like your password to include numbers?")
-    if (numeric) {
-      setPassArr = setPassArr + setPassArr.concat(numbericArr);
-      alert("You choose number.");
-    } else {
-      alert("You didn't choose number.")
-    }
+  // prompt to ask for numeric
+  let numeric = confirm("Would you like your password to include numbers?")
+  if (numeric) {
+    setPassArr = setPassArr.concat(numbericArr);
+    // number is selected
+    let numbReq = numbericArr[Math.floor(Math.random() * numbericArr.length)];
+    charReq = charReq.concat(numbReq);
+    charReqLength++;
+    alert("You choose number.");
+  } else {
+    alert("You didn't choose number.")
+  }
 
-    // prompt to ask for special characters
-    let specialCharacters = confirm("Would you like your password to include special characters?")
-    if (specialCharacters) {
-      setPassArr = setPassArr + setPassArr.concat(specialCharactersArr);
-      alert("You choose special characters.");
-    } else {
-      alert("You didn't choose special characters.")
-    }
+  // prompt to ask for special characters
+  let specialCharacters = confirm("Would you like your password to include special characters?")
+  if (specialCharacters) {
+    setPassArr = setPassArr.concat(specialCharactersArr);
+    // special character is selected
+    let specialCharReq = specialCharactersArr[Math.floor(Math.random() * specialCharactersArr.length)];
+    charReq = charReq.concat(specialCharReq);
+    charReqLength++;
+    alert("You choose special characters.");
+  } else {
+    alert("You didn't choose special characters.")
+  }
 
     // To break out of the loop if at least one of the criteria is true
     if (lowercase || uppercase || numeric || specialCharacters) {
@@ -127,24 +156,22 @@ function generatePassword(){
 
   }
 
-
-
-
-
-  
-
-
-
-
-  for (let i = 0; i < passwordLength; i++) {
+  // generate password minus 1 for any criteria selected
+  for (let i = 0; i < passwordLength - charReqLength; i++) {
     passwordchar = passwordchar + setPassArr[Math.floor(Math.random() * setPassArr.length)];
   }
 
+  // Adding the criteria characters to the main generate password
+  let fullPass = passwordchar + charReq;
 
-  randomPassword = passwordchar;  
+  // Shuffling the fullPass
+  let shuffle = fullPass.split('').sort(function(){return 0.5-Math.random()}).join('');
+
+  // making randomPassword to equal the shuffle password
+  randomPassword = shuffle;  
 
 
-
+// recall randomPassword so it could be display into the password field
 return randomPassword;
 }
 
